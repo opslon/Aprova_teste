f1c969ebbb6db12df5c967baf728abea9adad059
Vitor Almeida Chaves <manyvac@gmail.com>
2020-03-11 16:11:11 -0300

    correção dos commands pip install

--- Aprova_test/install_dep.py
+++ Aprova_test/install_dep.py
@@ -1,17 +1,15 @@
 import sys
 import os
-cmd = 'pip install requests2'
-os.system(cmd)
 
-'''
-pip install -r wget
-pip install -r requests2
-pip install -r pandas
-pip install -r lxml
-pip install -r beautifulsoup4
-pip install -r selenium
-pip install -r ZipFile
-'''
+
+os.system('pip install wget')
+os.system('pip install requests2')
+os.system('pip install pandas')
+os.system('pip install lxml')
+os.system('pip install beautifulsoup4')
+os.system('pip install selenium')
+os.system('pip install ZipFile')
+
 
 '''
 import wget
