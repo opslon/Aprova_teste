3b20a3a4f2f865b9de5b2c6e0173b5f04e0c596c
Vitor Almeida Chaves <manyvac@gmail.com>
2020-03-11 11:26:37 -0300

    Separando projeto em pasta

---
+++ Aprova_test/OLD_plug_and_play.ps1
@@ -0,0 +1,25 @@
+ 
+# Abre o PowerShell no diretório que o arquivo plug_and_play.ps1 está
+
+cd /D "%~dp0"
+
+# Cria pasta para o arquivo de instalação
+
+mkdir Instaladores
+
+cd Instaladores
+
+# Baixa instalador do driver Gecko
+$url = "https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-win64.zip"
+$output = "$PSScriptRoot\geckodriver-v0.26.0-win64.zip"
+$start_time = Get-Date
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+#OR
+Start-BitsTransfer -Source $url -Destination $output -Asynchronous
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+
+cmd /k
